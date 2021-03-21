301c9e1e6e6923eff48b2c7528adc6e3e3c02963
YeonJung Kim <duswnd25@gmail.com>
2021-03-22 00:12:45 +0900

    feat: 오류 수정
    배경 이미지 세로
    길이 안맞는 문제 수정, 잘못된 key 문제 수정, deprecated 된 요소 삭제, 불필요한
    디렉토리 제거, 잘못된 grid 사용 수정 drawer 기본 선택 값 null로 되어있는 문제
    해결

---
+++ pages/skills.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-container fluid class="background" fill-height>
+    <v-card id="jumbo-card" width="100vw" class="pa-4">
+      <v-card-title class="justify-center display-1 font-italic mb-auto">
+        Skills
+      </v-card-title>
+      <v-container fluid>
+        <v-row>
+          <v-col
+            v-for="(item, index) in skills"
+            :key="index"
+            lg="4"
+            md="3"
+            sm="6"
+          >
+            <skill
+              :title="item.title"
+              :name="item.name"
+              :summary="item.summary"
+              :experimentYear="item.experimentYear"
+              :projectCount="item.projectCount"
+            ></skill
+          ></v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import skill from "@/components/skill";
+export default {
+  components: {
+    skill
+  },
+  data() {
+    return {
+      skills: [
+        {
+          title: "Django, ML, Data Crawiling",
+          name: "Python",
+          summary: "Web Develope, Machin Learing, Toy project, Web crawler.",
+          experimentYear: 3,
+          projectCount: 4
+        },
+        {
+          title: "Spring framework",
+          name: "Java",
+          summary: "Web Develope",
+          experimentYear: 1,
+          projectCount: 2
+        },
+        {
+          title: "Nuxt.js",
+          name: "Node.js",
+          summary: "Web Develope",
+          experimentYear: 1,
+          projectCount: 2
+        },
+        {
+          title: "MySQL, Oracle DB, SQLite",
+          name: "Database",
+          summary: "Realational DB",
+          experimentYear: 3,
+          projectCount: 4
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.background {
+  background: linear-gradient(
+      to right,
+      rgba(0, 0, 0, 0.33),
+      rgba(20, 20, 20, 0.7)
+    ),
+    url("~/assets/background/jerry.gif");
+  background-position: center center, center center;
+  background-repeat: no-repeat, no-repeat;
+  background-size: cover, cover;
+}
+</style>
