eedf49a1400ef2ccdf0f7c7666036581e8706635
kimbyungsoo <kimbyungsoo@users.noreply.github.com>
2021-03-22 09:28:46 +0000

    deploy: adaa63a01b0e6599a2589eabd90987f8a2dcc230

---
+++ _nuxt/345c549.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{402:function(n,e,t){"use strict";t.r(e);var l={},r=t(79),component=Object(r.a)(l,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("h1",[n._v("work")])}),[],!1,null,null,null);e.default=component.exports}}]);
